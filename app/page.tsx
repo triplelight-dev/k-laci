1176aa3bc1ab34f2e734651869cee365bbfa0407
powellstreet <codestates.jc@gmail.com>
2025-06-05 17:52:23 +0900

    feat: home_intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 import Image from 'next/image';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import IntroSection from './home/IntroSection';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
 
-      <main className="container mx-auto flex-grow p-4">
+      <main className="container mx-auto flex-grow p-4" style={{ paddingTop: '3rem' }}>
         {/* Section 1 */}
-        <section className="my-8 p-4">
-          <h2 className="mb-4 text-2xl font-bold">Section 1 Title</h2>
-          <p>This is the content for section 1.</p>
-          {/* Add more content for section 1 */}
-        </section>
+        <IntroSection />
+        <IntroSection />
+
 
         {/* Section 2 */}
         <section className="my-8 p-4">
