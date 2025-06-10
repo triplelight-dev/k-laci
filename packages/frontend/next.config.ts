a256310188d1c304eef3a67570ec7bbc6301019e
powellstreet <codestates.jc@gmail.com>
2025-06-10 16:53:01 +0900

    feat: module import fix

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveAlias: {
+      atoms: './atoms'
+    }
+  }
 };
 
 export default nextConfig;
