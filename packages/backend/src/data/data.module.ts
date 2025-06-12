ce9564858a7691ea0c7a8781422639c0b109b526
powellstreet <codestates.jc@gmail.com>
2025-06-12 14:57:01 +0900

    feat: apply caching to region related apis

--- packages/backend/src/data/data.module.ts
+++ packages/backend/src/data/data.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DataService } from './data.service';
 import { DataController } from './data.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, CacheModule.register({ ttl: 300, max: 100 })],
   controllers: [DataController],
   providers: [DataService],
   exports: [DataService],
