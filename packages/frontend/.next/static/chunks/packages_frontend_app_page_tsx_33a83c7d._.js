e9346c07f0759ccf420477818219f5b6ad162b5f
powellstreet <codestates.jc@gmail.com>
2025-06-17 16:18:15 +0900

    chore: remove .next directory from git

--- packages/frontend/.next/static/chunks/packages_frontend_app_page_tsx_33a83c7d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_65181c2a._.js"
-],
-    source: "dynamic"
-});
--- packages/frontend/.next/static/chunks/packages_frontend_app_results_page_tsx_33a83c7d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/packages_frontend_efd12166._.js",
-  "static/chunks/node_modules_8eea717b._.js"
-],
-    source: "dynamic"
-});
