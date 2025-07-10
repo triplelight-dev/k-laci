08253afc2bc962a60a5dfce06b90ac795c438165
SEONGHO.BAK <psh30095@gmail.com>
2025-07-10 11:03:16 +0900

    fix: apply design qa -2

--- packages/frontend/components/atoms/circle/KlaciCodeCirclesMini.tsx
+++ packages/frontend/components/atoms/circle/KlaciCodeCirclesMini.tsx
@@ -55,7 +55,7 @@ const KlaciCodeCirclesMini: React.FC<KlaciCodeCirclesMiniProps> = ({
             fontWeight,
           }}
         >
-          {item.code.toUpperCase()}
+          {item.code}
         </div>
       ))}
     </div>
