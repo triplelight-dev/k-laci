3d93d01c1d0998c97a0aa13dca25536e49b63087
SEONGHO.BAK <psh30095@gmail.com>
2025-07-13 13:06:24 +0900

    fix: 다크모드 처리

--- packages/frontend/providers/ChakraProvider.tsx
+++ packages/frontend/providers/ChakraProvider.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 
 export default function ChakraProvider({ children }: { children: React.ReactNode }) {
   return <Provider value={defaultSystem}>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       {children}
     </ThemeProvider>
   </Provider>;
