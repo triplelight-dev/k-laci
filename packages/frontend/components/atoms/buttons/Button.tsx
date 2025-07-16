697411ccc703d00607bc182c331161df0c651630
powellstreet <dev@triplelight.co>
2025-07-16 16:31:03 +0900

    feat: header button css

--- packages/frontend/components/atoms/buttons/Button.tsx
+++ packages/frontend/components/atoms/buttons/Button.tsx
@@ -1,5 +1,5 @@
-import { Flex } from "@chakra-ui/react";
-import { useState } from "react";
+import { Flex } from '@chakra-ui/react';
+import { useState } from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string | React.ReactNode;
@@ -16,20 +16,31 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function Button(props: ButtonProps) {
-  const { width, label, icon, variant = 'primary', theme = 'dark', onClick = () => { }, padding = '12px 30px', fontSize = '18px', fontWeight = '400', borderRadius = '14px' } = props;
+  const {
+    width,
+    label,
+    icon,
+    variant = 'primary',
+    theme = 'dark',
+    onClick = () => {},
+    padding = '12px 30px',
+    fontSize = '18px',
+    fontWeight = '400',
+    borderRadius = '10px',
+  } = props;
   const [isHovered, setIsHovered] = useState(false);
 
   const darkThemeStyle = {
     backgroundColor: variant === 'primary' ? '#ffffff' : 'transparent',
     color: variant === 'primary' ? '#000000' : '#ffffff',
     border: variant === 'primary' ? 'none' : '1px solid #ffffff',
-  }
+  };
 
   const lightThemeStyle = {
     backgroundColor: variant === 'primary' ? '#000000' : 'transparent',
     color: variant === 'primary' ? '#ffffff' : '#000000',
     border: variant === 'primary' ? 'none' : '1px solid #000000',
-  }
+  };
 
   // 호버 시 스타일 계산
   const getHoverStyle = () => {
@@ -46,38 +57,50 @@ export default function Button(props: ButtonProps) {
     }
   };
 
-  return <button
-    onClick={onClick}
-    onMouseEnter={() => setIsHovered(true)}
-    onMouseLeave={() => setIsHovered(false)}
-    style={{
-      display: 'flex',
-      alignItems: 'center',
-      gap: '8px',
-      padding: padding,
-      width: width ? width : 'auto',
-      borderRadius: borderRadius,
-      fontSize: fontSize,
-      fontWeight: fontWeight,
-      cursor: 'pointer',
-      transition: 'all 0.2s ease',
-      ...(theme === 'dark' ? darkThemeStyle : lightThemeStyle),
-      ...(isHovered ? getHoverStyle() : {}),
-      ...(props.style ? props.style : {}),
-    }}
-  >
-    <Flex gap='8px' alignItems='center' justifyContent='center' cursor={'pointer'} style={{
-      textAlign: 'center',
-    }}>
-      {label}
-      {icon && (
-        <div style={{
-          transition: 'all 0.2s ease',
-          filter: isHovered ? 'brightness(0)' : 'none'
-        }}>
-          {icon}
-        </div>
-      )}
-    </Flex>
-  </button>;
+  return (
+    <button
+      onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: '8px',
+        padding: padding,
+        width: width ? width : 'auto',
+        minWidth: '110px',
+        justifyContent: 'center',
+        borderRadius,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        cursor: 'pointer',
+        transition: 'all 0.2s ease',
+        ...(theme === 'dark' ? darkThemeStyle : lightThemeStyle),
+        ...(isHovered ? getHoverStyle() : {}),
+        ...(props.style ? props.style : {}),
+      }}
+    >
+      <Flex
+        gap="8px"
+        alignItems="center"
+        justifyContent="center"
+        cursor={'pointer'}
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        {label}
+        {icon && (
+          <div
+            style={{
+              transition: 'all 0.2s ease',
+              filter: isHovered ? 'brightness(0)' : 'none',
+            }}
+          >
+            {icon}
+          </div>
+        )}
+      </Flex>
+    </button>
+  );
 }
