12787bb62c234973d99df847a4409a218663faf2
블루썸 <dev@triplelight.co>
2025-10-29 19:53:08 +0900

    RegionCard 변수 값 오류 수정

--- packages/frontend/features/home/components/RankCardSlider.tsx
+++ packages/frontend/features/home/components/RankCardSlider.tsx
@@ -411,6 +411,7 @@ export default function RankCardSlider() {
                       pointerEvents: 'auto',
                     }}
                     isHideBadge={true}
+                    mobile={false}
                   />
                 )}
               </div>
