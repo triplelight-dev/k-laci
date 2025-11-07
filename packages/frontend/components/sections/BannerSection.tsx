0bac2d05edbbc1501b9a118ac88d385da0823946
블루썸 <dev@triplelight.co>
2025-11-07 11:15:43 +0900

    모바일 지정 오류 수정

--- packages/frontend/components/sections/BannerSection.tsx
+++ packages/frontend/components/sections/BannerSection.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { useIsMobile } from '@/hooks';
 import { useDisclosure } from '@chakra-ui/react';
 import BannerModal from '../ui/BannerModal';
 
 export default function BannerSection() {
+  const isMobile = useIsMobile();
   const { open: isOpen, onOpen, onClose } = useDisclosure({ defaultOpen: true });
 
-  return <BannerModal isOpen={isOpen} onClose={onClose} />;
+  return <BannerModal isOpen={isOpen} onClose={onClose} mobile={isMobile} />;
 }
